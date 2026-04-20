f9de2a617b74b2c4feb2beaba87405e2d7c20ef2
Liam Bendiksen <liam.bendiksen@gmail.com>
2026-04-20 19:48:58 +0200

    First commit

---
+++ Lab2/src/aod/lab2/Tree.java
@@ -0,0 +1,14 @@
+package aod.lab2;
+
+public interface Tree<T> {
+	
+	public void add(T item);
+	
+	public boolean searchFor(T itemToSearchFor);
+	
+	public int size();
+	
+	public void clear();
+	
+	public boolean remove(T itemToRemove);
+}
